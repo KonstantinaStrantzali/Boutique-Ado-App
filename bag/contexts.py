f69228f8eaa8dcc7a579c34cdcb4707752aa09b8
KonstantinaStrantzali <konstrantz@gmail.com>
2022-05-20 08:49:52 +0000

    added stripe payments

--- bag/contexts.py
+++ bag/contexts.py
@@ -40,6 +40,7 @@ def bag_contents(request):
         free_delivery_delta = 0
     
     grand_total = delivery + total
+
     
     context = {
         'bag_items': bag_items,
@@ -52,3 +53,4 @@ def bag_contents(request):
     }
 
     return context
+
